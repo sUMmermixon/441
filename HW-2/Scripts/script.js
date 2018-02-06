12c15220955a35857582835e6f7eaf8fb078a575
Summer Mixon <31555135+sUMmermixon@users.noreply.github.com>
2018-02-06 13:18:29 -0700

    file rework

--- HW-2/Scripts/script.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello World!");
-document.write("<h1>Hello World!</h1><p>You are seeeing this thanks to JavaScript!");
---
+++ HW-2/script.js
@@ -0,0 +1,2 @@
+document.write("<h1>Hello World!</h1>");
+document.write("<p>You are seeeing this thanks to JavaScript!</p>");
